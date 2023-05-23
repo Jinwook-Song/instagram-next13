97c34b26aeb7b7d8e3a94cad34c9c7efbb0bc5e0
jinwook song <wlsdnr129@naver.com>
2023-05-23 15:46:16 +0900

    custom signin & redirect

--- src/components/ui/ColorButton.tsx
+++ src/components/ui/ColorButton.tsx
@@ -1,13 +1,24 @@
+import { cls } from '@/libs/client/util';
+
 type Props = {
   text: string;
   onClick: () => void;
+  size?: 'sm' | 'lg';
 };
 
-export default function ColorButton({ text, onClick }: Props) {
+export default function ColorButton({ text, onClick, size = 'sm' }: Props) {
   return (
-    <div className='rounded-md bg-gradient-to-br from-amber-300 via-rose-500 to-fuchsia-600 p-[2px] overflow-hidden cursor-pointer'>
+    <div
+      className={cls(
+        'rounded-md bg-gradient-to-br from-amber-300 via-rose-500 to-fuchsia-600 overflow-hidden cursor-pointer',
+        size === 'sm' ? 'p-[2px]' : 'p-1'
+      )}
+    >
       <button
-        className='bg-white rounded-[4px] text-base px-1 py-[2px] hover:bg-opacity-30 transition-all hover:text-white'
+        className={cls(
+          'bg-white rounded-[4px] text-base hover:bg-opacity-30 transition-all hover:text-white',
+          size === 'sm' ? 'px-1 py-[2px]' : 'px-2 py-1 text-2xl'
+        )}
         onClick={onClick}
       >
         {text}
