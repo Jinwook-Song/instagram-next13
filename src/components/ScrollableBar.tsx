d0130c66f51acdc12dfef45e1b37d6077b7f881c
jinwook song <wlsdnr129@naver.com>
2023-05-25 23:46:40 +0900

    style: multi carousel

---
+++ src/components/ScrollableBar.tsx
@@ -0,0 +1,28 @@
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+const responsive = {
+  desk: {
+    breakpoint: { max: 4000, min: 576 },
+    items: 6,
+  },
+  tablet: {
+    breakpoint: { max: 576, min: 464 },
+    items: 5,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 4,
+  },
+};
+
+export default function ScrollableBar({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <Carousel containerClass='w-full flex gap-2' responsive={responsive}>
+      {children}
+    </Carousel>
+  );
+}
