6dc7d67598c3be0f57c686e9bc321b8f7958f49e
jinwook song <wlsdnr129@naver.com>
2023-05-28 23:41:35 +0900

    feat: force dynamic

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,6 +1,9 @@
 import { searchUsers } from '@/libs/server/user';
 import { NextResponse } from 'next/server';
 
+// 요청할때마다 항상 새로 실행되도록 (static 하지 않게)
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
   return searchUsers().then((data) => NextResponse.json(data));
 }
